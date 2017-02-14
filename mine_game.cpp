af8899eb5ad08456058337de5207377c6a4956e7
Kritika <kritikagr12@gmail.com>
2017-02-14 17:05:37 +0530

    sound added

--- mine_game.cpp
+++ mine_game.cpp
@@ -4,11 +4,13 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <pthread.h>
+#include<string.h>
 #include <queue>
 #include <cmath>
 #include <time.h>
 #include <stdlib.h>
-
+#include <ao/ao.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <SOIL/SOIL.h>
@@ -23,6 +25,7 @@ using namespace std;
 
 #define pb push_back
 #define mp make_pair
+static const int BUF_SIZE = 4096;
 
 struct VAO {
 		GLuint VertexArrayID;
@@ -49,6 +52,20 @@ struct COLOR {
 		float r;
 		float g;
 		float b;
+};
+struct WavHeader {
+    char id[4]; //should contain RIFF
+    int32_t totalLength;
+    char wavefmt[8];
+    int32_t format; // 16 for PCM
+    int16_t pcm; // 1 for PCM
+    int16_t channels;
+    int32_t frequency;
+    int32_t bytesPerSecond;
+    int16_t bytesByCapture;
+    int16_t bitsPerSample;
+    char data[4]; // "data"
+    int32_t bytesInData;
 };
 //defining colors
 COLOR grey = {168.0/255.0,168.0/255.0,168.0/255.0};
@@ -109,12 +126,13 @@ int block_rotating_degree=6;
 float cuboid_rotation_angle=(float)block_rotating_degree * M_PI/180;
 int no_of_rotations=90/block_rotating_degree;
 float BlockFallingSpeed=0.08;
+pthread_t Writer_thr[3];
 bool helicam=0,mousefollow=0;
 GLFWwindow* window;
 queue<glm::vec3> block_last_pos;
 
 glm::vec3 win_tile=glm::vec3(0,0,0);
-bool game_won=0;
+bool game_won=0,lost=0;
 double game_winning_time;
 int Level=1;
 /* Function to load Shaders - Use it as it is */
@@ -206,12 +224,75 @@ void quit(GLFWwindow *window)
 		glfwTerminate();
 		exit(EXIT_SUCCESS);
 }
+void* Writer(void * i)
+{
+	ao_device* device;
+    ao_sample_format format;
+    int defaultDriver;
+    WavHeader header;
+
+    std::ifstream file;
+    file.open("explosion.wav", std::ios::binary | std::ios::in);
+
+    file.read(header.id, sizeof(header.id));
+    //assert(!std::memcmp(header.id, "RIFF", 4)); //is it a WAV file?
+    file.read((char*)&header.totalLength, sizeof(header.totalLength));
+    file.read(header.wavefmt, sizeof(header.wavefmt)); //is it the right format?
+    //assert(!std::memcmp(header.wavefmt, "WAVEfmt ", 8));
+    file.read((char*)&header.format, sizeof(header.format));
+    file.read((char*)&header.pcm, sizeof(header.pcm));
+    file.read((char*)&header.channels, sizeof(header.channels));
+    file.read((char*)&header.frequency, sizeof(header.frequency));
+    file.read((char*)&header.bytesPerSecond, sizeof(header.bytesPerSecond));
+    file.read((char*)&header.bytesByCapture, sizeof(header.bytesByCapture));
+    file.read((char*)&header.bitsPerSample, sizeof(header.bitsPerSample));
+    file.read(header.data, sizeof(header.data));
+    file.read((char*)&header.bytesInData, sizeof(header.bytesInData));
+
+    ao_initialize();
+
+    defaultDriver = ao_default_driver_id();
+
+    memset(&format, 0, sizeof(format));
+    format.bits = header.format;
+    format.channels = header.channels;
+    format.rate = header.frequency;
+    format.byte_format = AO_FMT_LITTLE;
+
+    device = ao_open_live(defaultDriver, &format, NULL);
+    if (device == NULL) {
+        std::cout << "Unable to open driver" << std::endl;
+        //return;
+    }
 
 
+    char* buffer = (char*)malloc(BUF_SIZE * sizeof(char));
+
+    // determine how many BUF_SIZE chunks are in file
+    int fSize = header.bytesInData;
+    int bCount = fSize / BUF_SIZE;
+
+    for (int i = 0; i < bCount; ++i) {
+        file.read(buffer, BUF_SIZE);
+				if(lost==1) {ao_close(device);
+		    ao_shutdown();pthread_exit((void *)NULL);}
+        ao_play(device, buffer, BUF_SIZE);
+    }
+
+    int leftoverBytes = fSize % BUF_SIZE;
+  //  std::cout << leftoverBytes;
+    file.read(buffer, leftoverBytes);
+    memset(buffer + leftoverBytes, 0, BUF_SIZE - leftoverBytes);
+		if(lost==1) {ao_close(device);
+    ao_shutdown();pthread_exit((void *)NULL);}
+    ao_play(device, buffer, BUF_SIZE);
+
+    ao_close(device);
+    ao_shutdown();
+
+}
 
 
-
-/* Generate VAO, VBOs and return VAO handle */
 struct VAO* create3DObject (GLenum primitive_mode, int numVertices, const GLfloat* vertex_buffer_data, const GLfloat* color_buffer_data, GLenum fill_mode=GL_FILL)
 {
 		struct VAO* vao = new struct VAO;
@@ -966,6 +1047,7 @@ vector<vector<int> > read_floor(int level)
 		if(!in.is_open())
 		{
 				cout<<"Unable to open Level file"<<endl ;
+					pthread_join(Writer_thr[1],NULL);
 				exit(0) ;
 		}
 		in>>floor_width>>floor_length;
@@ -1247,6 +1329,7 @@ int main (int argc, char** argv)
 
 		initGL (window, width, height);
 		last_update_time = glfwGetTime();
+		pthread_create(&Writer_thr[1],NULL,Writer,(void*) NULL);
 		/* Draw in loop */
 		while (!glfwWindowShouldClose(window)) {
 
@@ -1262,7 +1345,7 @@ int main (int argc, char** argv)
 				// Poll for Keyboard and mouse events
 				glfwPollEvents();
 		}
-
+			pthread_join(Writer_thr[1],NULL);
 		glfwTerminate();
 		//    exit(EXIT_SUCCESS);
 }
